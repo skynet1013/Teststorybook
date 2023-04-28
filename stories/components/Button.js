77b5e417e62f12f27d0417155bc6444b9c3c0f0b
谷田　大空 <tanidataisei@yadaoozoranoMacBook-Pro.local>
2023-04-29 04:32:13 +0900

    testGit

---
+++ stories/components/Button.js
@@ -0,0 +1,21 @@
+import './button.css';
+
+export const createButton = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor = '#F5F5F5', // デフォルト値を正しく設定
+  label,
+  onClick,
+}) => {
+  const btn = document.createElement('button');
+  btn.type = 'button';
+  btn.innerText = label;
+  btn.addEventListener('click', onClick);
+
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  btn.className = ['storybook-button', `storybook-button--${size}`, mode].join(' ');
+
+  btn.style.backgroundColor = backgroundColor;
+
+  return btn;
+};
